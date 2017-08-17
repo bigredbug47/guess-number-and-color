b4e0fd558c827806d2c31860148f1db65afd83a7
bigredbug <31086329+bigredbug47@users.noreply.github.com>
2017-08-17 10:24:19 +0700

    Add files via upload

---
+++ Guess_Color.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guess_Color
+{
+    class Program
+    {
+        static int iCount = 0, iPosi = 0;
+        static double iMin, iTemp_Min;
+        static List<int> Width_Arr = new List<int>();
+        static List<int> Height_Arr = new List<int>();
+        static List<string> Color_Arr = new List<string>();
+        static void Main(string[] args)
+        {
+            string choose;
+            string user_w, user_h;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Your choice is:\n1. Input a card.\n2. Guess a card.\n3. Exit");
+                choose = Console.ReadLine();
+                Console.Clear();
+                if (choose == "1")
+                {
+                    int time;
+                    Console.WriteLine("How many card you will input ???\n\n");
+                    time = int.Parse(Console.ReadLine());
+                    for (int j = 0; j < time; j++)
+                    {
+                        Console.WriteLine("Card number " + (j + 1) + ":");
+                        input();
+                    }
+                }
+                else if (choose == "2" && iCount > 2)
+                {
+                    user_w = Console.ReadLine();
+                    user_h = Console.ReadLine();
+                    Guess_Color(int.Parse(user_w), int.Parse(user_h));
+                    Console.WriteLine("Do you want continue?\n1. Yes.\n2. No.\n");
+                    int take;
+                    take = int.Parse(Console.ReadLine());
+                    if (take == 1) choose = "1";
+                    else choose = "3";
+                }
+                else if (choose == "3")
+                {
+                    Console.WriteLine("\n\nThanks for playing!!!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Please choose correct number and the total of cards must larger than 2.\n\n");
+                    choose = "4";
+                }
+            } while (choose !="3");            
+        }
+        static void input()
+        {
+            string width, height, color;
+            width = Console.ReadLine();
+            height = Console.ReadLine();
+            color = Console.ReadLine();
+            Width_Arr.Add(int.Parse(width));
+            Height_Arr.Add(int.Parse(height));
+            Color_Arr.Add(color);
+            iCount++;
+        }
+        static void Guess_Color(int user_width, int user_height)
+        {
+            
+            iMin = Math.Sqrt(Math.Abs((double)((user_width - Width_Arr[0]) * (user_width - Width_Arr[0])-((user_height-Height_Arr[0])*(user_height-Height_Arr[0])))));
+            for (int i = 0; i < iCount; i++)
+            {
+                iTemp_Min = Math.Sqrt(Math.Abs(((double)((user_width - Width_Arr[i]) * (user_width - Width_Arr[i]) - ((user_height - Height_Arr[i]) * (user_height - Height_Arr[i]))))));
+                if (iTemp_Min <= iMin)
+                {
+                    iMin = iTemp_Min;
+                    iPosi = i;
+                }
+                Console.WriteLine("Number: " + i + " .Card's width: " + Width_Arr[i] + " .Card's height: " + Height_Arr[i] + " .Card's color: " + Color_Arr[i] + " .Distance: " + iTemp_Min);
+            }
+            Console.WriteLine("Your card's width is: " + user_width + " .Your card's height: " + user_height + " .Your card's color is maybe: " + Color_Arr[iPosi] + "\n");
+        }
+    }
+}
---
+++ Guess_Number.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nums_Guess
+{
+    class Program
+    {
+        static int range = 20;
+        static void Main(string[] args)
+        {
+            string khbit;
+            do
+            {
+                int[] nums = new int[1100];
+                int posi = 0;
+                int i = 1100;
+                int count_true = 1;
+                int count_false = 1;
+                float accuracy_true;
+		Console.WriteLine("Your input: ");
+                do
+                {
+                    input(ref nums, ref posi, ref count_true, ref count_false);
+                    accuracy_true = (float)count_true / (count_true + count_false);
+                    accuracy_true *= 100;
+                    if (posi == 10 || posi == 50 || posi == 100 || posi == 200 || posi == 500 || posi == 700 || posi == 1000)
+                    {
+                        Console.WriteLine("Range: " + range + " .Time: " + posi);
+                        Console.WriteLine("Accuracy(true): "+ accuracy_true + ".False: " + (100 - accuracy_true) + "\n\n\n");
+                    }
+                    i--;
+                } while (i > 0);
+                khbit = Console.ReadLine();
+            } while (khbit!="0");
+        }
+        static void input(ref int[] nums, ref int posi, ref int count_true, ref int count_false)
+        {
+            Random rand = new Random();
+            string str;
+            int user_num = -1;
+            int comp_num = -1;
+            comp_num = Guess_Number(nums, posi);
+            str = Console.ReadLine();
+            int.TryParse(str, out user_num);
+            if (user_num != -1) { nums[posi] = user_num; posi++; }
+            if (comp_num == user_num) count_true++;
+            else count_false++;
+        }
+        static int Guess_Number(int[] nums, int posi)
+        {
+            Random rand = new Random();
+            if (posi < range) return rand.Next(2);
+            else
+            {
+                posi -= 1;
+                int check = posi;
+                int[] temp = new int[range];
+                for (int i = range-1; i >= 0; i--) 
+                {
+                    temp[i] = nums[posi];
+                    posi--;
+                }
+                return CompareArr(nums, temp, check);
+            }
+        }
+        static int CompareArr(int[] nums, int[] temp, int posi)
+        {
+            int k = -1;
+            int count_zero, count_one;
+            count_one = count_zero = 0;
+            for (int i = 0; i < posi; i++) 
+            {
+                k = i;
+                for (int j = 0; j < range; j++) 
+                {
+                    if (temp[j] == nums[i] && i < posi){ i++; }
+                    else { j = range; i = k; }
+                    if (j == range - 1) 
+                    {
+                        if (nums[i] == 0) count_zero++;
+                        else count_one++;
+                    }
+                }
+            }
+            if (count_zero > count_one) return 0;
+            else return 1;
+        }
+    }
+}
